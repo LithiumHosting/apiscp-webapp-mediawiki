094a975311d455aa26259c1b794b3cd4b0a8d442
Troy Siedsma <tsiedsma@lithiumhosting.com>
2022-10-01 16:02:36 -0500

    Add install checks Enable updates Fix installation issues Allow setting admin username and site name during install

--- Handler.php
+++ Handler.php
@@ -12,16 +12,14 @@ class Handler extends Unknown
 
 	const DEFAULT_FORTIFICATION = 'max';
 	const FEAT_ALLOW_SSL        = true;
-	const FEAT_RECOVERY         = false;
-	const FEAT_GIT              = false;
 
 	public function display(): bool
 	{
-		return version_compare($this->php_version(), '7.2', '>=');
+		return version_compare($this->php_version(), '7', '>=');
 	}
 
 	public function hasUpdate(): bool
 	{
-		return false;
+		return true;
 	}
 }
